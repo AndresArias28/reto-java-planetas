bcd6a12b0bc0f11fa248dc9170ac9443e6af65f7
andres naranjo <santo2828@gmail.com>
2024-11-22 21:07:46 -0500

    modificacion metodo showDataPlants

--- src/App.java
+++ src/App.java
@@ -1,21 +1,29 @@
 import java.util.Scanner;
 
 public class App {
-
     static Scanner scanner = new Scanner(System.in);
-    static double[] distancias = { 54_600_000, 590_000_000, 1_200_000_000 }; // km
-    // static double[] oxygenResources = {12.7 ,54.0 , 78.3};
-    // static double[] foodResources = { 10.0, 20.0, 30.0 };
-    // static double[] waterResources = { 10.0, 20.0, 30.0 };
+    static double[] distances = { 54_600_000, 590_000_000, 1_200_000_000 }; // km
+    // planets description
+    static String[] descriptionPlanets = { 
+        "Marte es el llamado planeta rojo en la mitología romana, seleccionado por su color rojizo, este planeta es el cuarto en el sistema solar y el segundo más pequeño después de Mercurio",
+        " Júpiter es el planeta mas grande del sistema solar y el segundo mas grande despues de Marte",
+        "Saturno es el planeta mas grande del sistema solar y el tercer mas grande despues de Júpiter"
+     };
+    // Datos de nave
+    static String[] ships = { "Falcon 9", "Starship", "SLS" };
+    static double[] velocitysShip = { 27_000, 40_000, 29_000 }; // en km/h
+    static int[] capacidadPasajeros= { 2, 4, 3};
+    // Datos de planetas
+    static String[]   planets= { "Marte", "Jupiter", "Saturno"};
     static double velocity = 100_000; // km/h
-    static double averageOxygen = 550; // litros/dia
-    static double averageFood = 1.7; // kg/dia
-    static double averageWater = 2.5; // litros/dia
-    static double averageGas = 350; // Ton/dia
+    static double[] averages ={550, 1.7, 2.5, 350 };//{litros/dia, kg/dia , litros/dia,  Ton/dia}
+    //ajustar recursos
     static double litrosOxigeno = 0;
     static double toneladasCombustible = 0;
-
     static boolean makeAdjust = false;
+    //ajustes seleccionddaos
+    static int shipSelected = 0;
+    static int planetSelected = 0;
 
     public static void main(String[] args) throws Exception {
         Menu();
@@ -71,46 +79,42 @@ public class App {
         String confirm;
         switch (planet) {
             case 1:
-                System.out.println("estas seguro de selecionar Marte? (si/no)");
+                System.out.println("estas seguro de selecionar " + planets[planet-1] + " (si/no)");
                 confirm = scanner.next();
 
                 if (confirm.equals("si")) {
-                    showDataMars();
+                    showDataPlanets(planet);
 
                 } else {
                     System.out.println("ingresaste mal la opción o excogiste NO");
                 }
                 break;
+
             case 2:
                 System.out.println("estas seguro de selecionar Jupiter? (si/no)");
                 confirm = scanner.next();
                 if (confirm.equals("si")) {
-                    showDataJupiter();
+                    showDataPlanets(planet);
                 } else {
                     System.out.println("ingresaste mal la opción o excogiste NO");
                 }
-
                 break;
+
             case 3:
                 System.out.println("estas seguro de selecionar Saturno? (si/no)");
                 confirm = scanner.next();
 
                 if (confirm.equals("si")) {
-                    showDataSaturn();
+                    showDataPlanets(planet);
                 } else {
                     System.out.println("ingresaste mal la opción o excogiste NO");
                 }
                 break;
 
             case 4:
-                System.out.println("revisar y ajustar recursos");
-                System.out.println("Cuantos litros de oxigeno desea agregar: ");
-                 litrosOxigeno = scanner.nextDouble();
-                System.out.println("Cuantas toneladas de combustible desea agregar: ");
-                 toneladasCombustible = scanner.nextDouble();
-                System.out.println("Ajuste exitoso");
-                makeAdjust = true;
+                adjustResource();
                 break;
+                
             case 5:
                 System.out.println("Has salido del programa");
                 break;
@@ -121,72 +125,59 @@ public class App {
         }
     }
 
-    private static void showDataMars() {
-        double estimateTime = calculateTime(distancias[0]);
-        System.out.println("Has seleccionado Marte");
-        System.out.println(  "Marte es el llamado plante rojo en la mitología romana, seleccionado por su color rojizo, este planeta es el cuarto en el sistema solar y el segundo más pequeño después de Mercurio");
-        System.out.println("La distancia a Marte es: " + distancias[0] + " km");
-        System.out.printf( "El tiempo de viaje a una velocidad de %.0f km/h tarda %.2f días%n", velocity, estimateTime);
-        System.out.println("Se necesitan los siguientes recursos para eñ viaje: ");
-        System.out.printf("Oxígeno: %.2f litros%n", calculateOxygen(estimateTime));
-        System.out.printf("Comida: %.2f kg%n", calculateFood(estimateTime));
-        System.out.printf("Agua: %.2f litros%n", calculateWater(estimateTime));
-        System.out.printf("combustible: %.2f Ton%n", calculateGas(estimateTime));
+    private static void adjustResource() {
+        System.out.println("revisar y ajustar recursos");
+        System.out.println("Cuantos litros de oxigeno desea agregar: ");
+        litrosOxigeno = scanner.nextDouble();
+        System.out.println("Cuantas toneladas de combustible desea agregar: ");
+        toneladasCombustible = scanner.nextDouble();
+        System.out.println("Ajuste exitoso");
+        makeAdjust = true;
     }
 
-    private static void showDataJupiter() {
-        double estimateTime = calculateTime(distancias[1]);
-        System.out.println("Has seleccionado Júpiter");
-        System.out.println("Júpiter es el planeta mas grande del sistema solar y el segundo mas grande despues de Marte");
-        System.out.println("La distancia a Júpiter es: " + distancias[1] + " km");
-        System.out.printf( "El tiempo de viaje a una velocidad de %.0f km/h tarda %.2f días%n", velocity, estimateTime);
-        System.out.println("Se necesitan los siguientes recursos para eñ viaje: ");
-        System.out.printf("Oxígeno: %.2f litros%n", calculateOxygen(estimateTime));
-        System.out.printf("Comida: %.2f kg%n", calculateFood(estimateTime));
-        System.out.printf("Agua: %.2f litros%n", calculateWater(estimateTime));
-    }
-
-    private static void showDataSaturn() {
-        double estimateTime = calculateTime(distancias[2]);
-        System.out.println("Has seleccionado Saturno");
-        System.out.println( "Saturno es el planeta mas grande del sistema solar y el tercer mas grande despues de Júpiter");
-        System.out.println("La distancia a Saturno es: " + distancias[2] + " km");
-        System.out.printf( "El tiempo de viaje a una velocidad de %.0f km/h tarda %.2f días%n", velocity, estimateTime);
-        System.out.println("Se necesitan los siguientes recursos para eñ viaje: ");
-        System.out.printf("Oxígeno: %.2f litros%n", calculateOxygen(estimateTime));
-        System.out.printf("Comida: %.2f kg%n", calculateFood(estimateTime));
-        System.out.printf("Agua: %.2f litros%n", calculateWater(estimateTime));
+    private static void showDataPlanets(int planet) {
+        double estimateTime = calculateTime(distances[planet-1]);
+        System.out.println("Has seleccionado planeta " + planets[planet-1]);
+        System.out.println( descriptionPlanets[planet-1] );
+        System.out.println("La distancia a "+planets[planet-1]+" es: " + distances[planet-1] + " km");
+       // System.out.printf("El tiempo de viaje a una velocidad de %.0f km/h tarda %.2f días%n", velocity, estimateTime);
+        System.out.println("Se necesitan los siguientes recursos por persona para el viaje: ");
+        System.out.printf("Oxígeno: %.2f litros%n", calculateResources(estimateTime, averages[0] ));
+        System.out.printf("Comida: %.2f kg%n", calculateResources(estimateTime,  averages[1]));
+        System.out.printf("Agua: %.2f litros%n", calculateResources(estimateTime,  averages[2]));
+        System.out.printf("combustible: %.2f Ton%n", calculateResources(estimateTime,  averages[3]));
+        planetSelected = planet;
 
     }
 
-    private static double calculateOxygen(double dias) {
-        return dias * averageOxygen;
-    }
-
-    private static double calculateFood(double dias) {
-        return dias * averageFood;
-    }
-
-    private static double calculateWater(double dias) {
-        return dias * averageWater;
-    }
-
-    private static double calculateGas(double dias) {
-        return dias * averageGas;
+    private static double calculateResources(double dias, double estimateAverage ){
+        return dias * estimateAverage;
     }
 
     private static double calculateTime(double distance) {
         double timeInHours = distance / velocity;
         double timeInDays = timeInHours / 24;
-        // double timeInMonths = timeInHours / 24 / 30;
-        // double timeInYears = timeInMonths / 12;
         return timeInDays;
 
     }
 
     public static void selectAirplanet() {
+        System.out.println("\n--- ships Disponibles ---");
+        for (int i = 0; i < ships.length; i++) {
+            System.out.printf("%d. %s (%.0f km/h)\n", i + 1, ships[i], velocitysShip[i]);
+        }
+        System.out.print("Selecciona una nave (1-3): ");
+        int naveIndex = scanner.nextInt() - 1;
 
-    }
+    //     if (naveIndex >= 0 && naveIndex < ships.length) {
+    //         shipseleccionada = ships[naveIndex];
+    //         velocidadNave = velocitysShip[naveIndex];
+    //         System.out.printf("Has seleccionado: %s (%.0f km/h)\n", shipseleccionada, velocidadNave);
+    //     } else {
+    //         System.out.println("Selección inválida.");
+        
+
+     }
 
     public static void calcularRecursos() {
 
